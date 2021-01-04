945b6b1ac09eecdca8273c2aaecf979b65926e06
galihhhp <pangestu.galihhhp@gmail.com>
2021-01-04 10:15:46 +0700

    hero meditation static section have been done

--- src/ProjectPages/Layout Sections/Hero/Meditation.jsx
+++ src/ProjectPages/Layout Sections/Hero/Meditation.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import MeditationNavbar from './MeditationNavbar';
 import './meditation.css';
@@ -11,19 +11,18 @@ export default function Meditation() {
     const title = {
         color: '#2E6888',
         fontWeight: 'bolder',
+        fontSize: '64px',
     };
 
     const body = {
         color: '#2E6888',
+        fontSize: '16px',
     };
 
     return (
-        <Fragment>
+        <div className='hero-wrapper'>
             <MeditationNavbar />
-            <Container
-                style={container}
-                className='hero-wrapper d-flex flex-column'
-                fluid>
+            <Container style={container} className=' d-flex flex-column'>
                 <h1 style={title} className='pt-5'>
                     Mindfulness Meditation
                 </h1>
@@ -36,6 +35,6 @@ export default function Meditation() {
                     <strong>Mindfulness improves mental health.</strong>
                 </p>
             </Container>
-        </Fragment>
+        </div>
     );
 }
